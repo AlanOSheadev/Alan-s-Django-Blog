8544472b80d45f0d48fb25ce78c5d5dac0336cca
Alan O Shea <alanosheaclash@gmail.com>
2020-03-16 12:54:56 +0000

    fixed errors found

--- posts/views.py
+++ posts/views.py
@@ -1,26 +1,27 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Post
-from .models import BlogPostFrom
+from .forms import BlogPostForm
 
 
 def get_posts(request):
     """
-    Create a view that will retun a list of posts
-    that were published prior to 'now' and render them to the
-    'Blogposts.html' template
+    Create a view that will return a list
+    of Posts that were published prior to 'now'
+    and render them to the 'blogposts.html' template
     """
-
-    posts = Post.objects.filter(published_date__lte=timezone.now
-                                ()).ordered_by('published_date')
+    posts = Post.objects.filter(published_date__lte=timezone.now()
+        ).order_by('-published_date')
     return render(request, "blogposts.html", {'posts': posts})
 
 
 def post_detail(request, pk):
     """
-    Create a view that returns a single Post Object
-    based on the post ID (pk) and render it to the
-    'postdetail.html' template. Or return a
+    Create a view that returns a single
+    Post object based on the post ID (pk) and
+    render it to the 'postdetail.html' template.
+    Or return a 404 error if the post is
+    not found
     """
     post = get_object_or_404(Post, pk=pk)
     post.views += 1
@@ -31,15 +32,15 @@ def post_detail(request, pk):
 def create_or_edit_post(request, pk=None):
     """
     Create a view that allows us to create
-    or edit a post depending if the Post Id
+    or edit a post depending if the Post ID
     is null or not
     """
     post = get_object_or_404(Post, pk=pk) if pk else None
     if request.method == "POST":
-        form = BlogPostFrom(request.POST, request.FILES, instance=post)
+        form = BlogPostForm(request.POST, request.FILES, instance=post)
         if form.is_valid():
             post = form.save()
             return redirect(post_detail, post.pk)
     else:
-        form = BlogPostFrom(instance=post)
+        form = BlogPostForm(instance=post)
     return render(request, 'blogpostform.html', {'form': form})
